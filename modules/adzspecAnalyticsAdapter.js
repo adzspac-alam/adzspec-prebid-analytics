45866c0ec7cb1f175c553ffa7014fdd1fd071da4
adzspac-alam <107699975+adzspac-alam@users.noreply.github.com>
2022-07-22 15:33:44 +0500

    DONE: added test code

--- modules/adzspecAnalyticsAdapter.js
+++ modules/adzspecAnalyticsAdapter.js
@@ -13,20 +13,14 @@ const {
   }
 } = CONSTANTS;
 
-let publisherId;
+const url = 'URL_TO_SERVER_ENDPOINT';
 
-let allEvents = {};
-let endpoint = 'https://publisher';
-let adzspecAnalyticsAdapter = Object.assign(adapter(
-  {
-    analyticsType
-  }),
-{
-  track({eventType, args}) {
-    if (typeof args !== 'undefined') {
-      adzspecAnalyticsAdapter.callEventHandler(eventType, args);
-    }
+let adzspecAnalyticsAdapter = Object.assign(adapter({url, analyticsType}), {
+  check() {
+    logInfo(analyticsType);
+    logInfo("start here analytics");
   }
+  // ... code ...
 });
 
 // save the base class function
@@ -34,58 +28,90 @@ adzspecAnalyticsAdapter.originEnableAnalytics = adzspecAnalyticsAdapter.enableAn
 
 // override enableAnalytics so we can get access to the config passed in from the page
 adzspecAnalyticsAdapter.enableAnalytics = function (config) {
-  if (!config.options.publisherId) {
-    logError('Publisher ID option is not defined.');
-    return;
-  }
   initOptions = config.options;
-  publisherId = initOptions.publisherId;
   adzspecAnalyticsAdapter.originEnableAnalytics(config);  // call the base class function
 };
 
-adzspecAnalyticsAdapter.callEventHandler = function (eventType, args) {
-  allEvents[eventType].push(args);
-  switch (eventType) {
-    case AUCTION_END:
-      handleAuctionEnd();
-      break;
-    case BID_WON:
-      handleBidWon(args);
-      break;
-  }
-}
 
-function handleAuctionEnd() {
 
-    try {
-      ajax(endpoint + '.adzspec.com/analytics/auctions/' + publisherId, callBack(), JSON.stringify(allEvents), {
-        contentType: 'application/json',
-        method: 'POST'
-      });
-    } catch (err) {
-      logError('Adzspec analytics request encounter an error: ', err);
-    }
-}
+// let allEvents = {};
+// let endpoint = 'https://publisher';
+// let adzspecAnalyticsAdapter = Object.assign(adapter(
+//   {
+//     analyticsType
+//   }),
+// {
+//   track({eventType, args}) {
+//     logInfo("event start here");
+//   logInfo(args);
+//   logInfo(eventType);
+//     if (typeof args !== 'undefined') {
+//       adzspecAnalyticsAdapter.callEventHandler(eventType, args);
+//     }
+//   }
+// });
 
-function handleBidWon(args) {
-  try {
-    ajax(endpoint + '.adzspec.com/analytics/bid_won/' + publisherId, callBack(), JSON.stringify(args), {
-      contentType: 'application/json',
-      method: 'POST'
-    });
-  } catch (err) {
-    logError('Adzspec analytics request encounter an error: ', err);
-  }
-}
+// // save the base class function
+// adzspecAnalyticsAdapter.originEnableAnalytics = adzspecAnalyticsAdapter.enableAnalytics;
 
-function callBack() {
-  logInfo('Analytics events sent successfully');
-}
+// // override enableAnalytics so we can get access to the config passed in from the page
+// adzspecAnalyticsAdapter.enableAnalytics = function (config) {
+//   if (!config.options.publisherId) {
+//     logInfo(config.publisherId);
+//     logError('Publisher ID option is not defined. from ruman');
+//     return;
+//   }
+//   initOptions = config.options;
+//   publisherId = initOptions.publisherId;
+//   adzspecAnalyticsAdapter.originEnableAnalytics(config);  // call the base class function
+// };
+
+// adzspecAnalyticsAdapter.callEventHandler = function (eventType, args) {
+//   logInfo("event start here");
+//   logInfo(args);
+//   logInfo(eventType);
+//   allEvents[eventType].push(args);
+//   switch (eventType) {
+//     case AUCTION_END:
+//       handleAuctionEnd();
+//       break;
+//     case BID_WON:
+//       handleBidWon(args);
+//       break;
+//   }
+// }
+
+// function handleAuctionEnd() {
+
+//     try {
+//       ajax(endpoint + '.adzspec.com/analytics/auctions/' + publisherId, callBack(), JSON.stringify(allEvents), {
+//         contentType: 'application/json',
+//         method: 'POST'
+//       });
+//     } catch (err) {
+//       logError('Adzspec analytics request encounter an error: ', err);
+//     }
+// }
+
+// function handleBidWon(args) {
+//   try {
+//     ajax(endpoint + '.adzspec.com/analytics/bid_won/' + publisherId, callBack(), JSON.stringify(args), {
+//       contentType: 'application/json',
+//       method: 'POST'
+//     });
+//   } catch (err) {
+//     logError('Adzspec analytics request encounter an error: ', err);
+//   }
+// }
+
+// function callBack() {
+//   logInfo('Analytics events sent successfully');
+// }
 
 adaptermanager.registerAnalyticsAdapter({
   adapter: adzspecAnalyticsAdapter,
   code: 'adzspecAnalytics',
-  gvlid: 99
+  gvlid: 1
 });
 
 export default adzspecAnalyticsAdapter;
